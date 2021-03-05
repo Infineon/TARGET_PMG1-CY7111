cf4f3d67276b4c17a1def8d1b6262ea50e7e5717
gitlab-runner <do-not-reply@cypress.com>
2021-03-05 12:55:53 -0600

    Upload TARGET_PMG1-CY7111 1.1.0.21063

---
+++ docs/html/search/pages_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['pmg1_2dcy7111_20bsp',['PMG1-CY7111 BSP',['../index.html',1,'']]],
+  ['peripheral_20default_20bsp_20settings',['Peripheral Default BSP Settings',['../md_source_bsps_psoc4_PMG1-CY7111_bsp_settings.html',1,'']]]
+];
