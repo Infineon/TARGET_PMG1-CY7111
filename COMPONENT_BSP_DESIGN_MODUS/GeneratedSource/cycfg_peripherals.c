795a2ec697b80589eef7e9055086d83742661aea
gitlab-runner <do-not-reply@cypress.com>
2021-10-14 14:49:42 -0500

    Upload TARGET_PMG1-CY7111 2.0.0.25183

--- COMPONENT_BSP_DESIGN_MODUS/GeneratedSource/cycfg_peripherals.c
+++ COMPONENT_BSP_DESIGN_MODUS/GeneratedSource/cycfg_peripherals.c
@@ -4,13 +4,14 @@
 * Description:
 * Peripheral Hardware Block configuration
 * This file was automatically generated and should not be modified.
-* Tools Package 2.2.0.2801
-* mtb-pdl-cat2-release-v1.1.0 1.1.0.2932
-* personalities 3.0.0.0
-* udd 3.0.0.929
+* Tools Package 2.4.0.5721
+* mtb-pdl-cat2 1.4.0.5338
+* personalities 5.0.0.0
+* udd 3.0.0.1428
 *
 ********************************************************************************
-* Copyright 2021 Cypress Semiconductor Corporation
+* Copyright 2021 Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.
 * SPDX-License-Identifier: Apache-2.0
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,40 +31,40 @@
 
 cy_stc_fault_vbus_ovp_cfg_t mtb_usbpd_port0_ovp_config = 
 {
-	.enable = true,
-	.mode = 2,
-	.threshold = 20,
-	.debounce = 10,
-	.retryCount = 2,
+    .enable = true,
+    .mode = 2,
+    .threshold = 20,
+    .debounce = 10,
+    .retryCount = 2,
 };
 cy_stc_fault_vbus_uvp_cfg_t mtb_usbpd_port0_uvp_config = 
 {
-	.enable = true,
-	.mode = 2,
-	.threshold = 20,
-	.debounce = 10,
-	.retryCount = 2,
+    .enable = true,
+    .mode = 2,
+    .threshold = 20,
+    .debounce = 10,
+    .retryCount = 2,
 };
 cy_stc_usbpd_config_t mtb_usbpd_port0_config = 
 {
-	.vbusOvpConfig = &mtb_usbpd_port0_ovp_config,
-	.vbusUvpConfig = &mtb_usbpd_port0_uvp_config,
-	.vbusOcpConfig = NULL,
-	.vbusScpConfig = NULL,
-	.vbusRcpConfig = NULL,
-	.vconnOcpConfig = NULL,
-	.ccOvpConfig = NULL,
-	.sbuOvpConfig = NULL,
-	.legacyChargingConfig = NULL,
+    .vbusOvpConfig = &mtb_usbpd_port0_ovp_config,
+    .vbusUvpConfig = &mtb_usbpd_port0_uvp_config,
+    .vbusOcpConfig = NULL,
+    .vbusScpConfig = NULL,
+    .vbusRcpConfig = NULL,
+    .vconnOcpConfig = NULL,
+    .ccOvpConfig = NULL,
+    .sbuOvpConfig = NULL,
+    .legacyChargingConfig = NULL,
 };
 
 
 void init_cycfg_peripherals(void)
 {
-	Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_RX, CY_SYSCLK_DIV_8_BIT, 0U);
-	Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_TX, CY_SYSCLK_DIV_8_BIT, 1U);
-	Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_SAR, CY_SYSCLK_DIV_8_BIT, 2U);
-	Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_REFGEN, CY_SYSCLK_DIV_8_BIT, 3U);
-	Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_FILTER1, CY_SYSCLK_DIV_8_BIT, 3U);
-	Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_ISNK, CY_SYSCLK_DIV_16_BIT, 0U);
+    Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_RX, CY_SYSCLK_DIV_8_BIT, 0U);
+    Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_TX, CY_SYSCLK_DIV_8_BIT, 1U);
+    Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_SAR, CY_SYSCLK_DIV_8_BIT, 2U);
+    Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_REFGEN, CY_SYSCLK_DIV_8_BIT, 3U);
+    Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_FILTER1, CY_SYSCLK_DIV_8_BIT, 3U);
+    Cy_SysClk_PeriphAssignDivider(PCLK_USBPD0_CLOCK_ISNK, CY_SYSCLK_DIV_16_BIT, 0U);
 }
--- COMPONENT_BSP_DESIGN_MODUS/GeneratedSource/cycfg_pins.h
+++ COMPONENT_BSP_DESIGN_MODUS/GeneratedSource/cycfg_pins.h
@@ -4,13 +4,14 @@
 * Description:
 * Pin configuration
 * This file was automatically generated and should not be modified.
-* Tools Package 2.2.0.2801
-* mtb-pdl-cat2-release-v1.1.0 1.1.0.2932
-* personalities 3.0.0.0
-* udd 3.0.0.929
+* Tools Package 2.4.0.5721
+* mtb-pdl-cat2 1.4.0.5338
+* personalities 5.0.0.0
+* udd 3.0.0.1428
 *
 ********************************************************************************
-* Copyright 2021 Cypress Semiconductor Corporation
+* Copyright 2021 Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.
 * SPDX-License-Identifier: Apache-2.0
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,7 +34,7 @@
 #include "cy_gpio.h"
 #include "cycfg_routing.h"
 #if defined (CY_USING_HAL)
-	#include "cyhal_hwmgr.h"
+    #include "cyhal_hwmgr.h"
 #endif //defined (CY_USING_HAL)
 
 #if defined(__cplusplus)
@@ -48,24 +49,24 @@ extern "C" {
 #define CYBSP_SWDCK_DRIVEMODE CY_GPIO_DM_STRONG
 #define CYBSP_SWDCK_INIT_DRIVESTATE 1
 #ifndef ioss_0_port_1_pin_0_HSIOM
-	#define ioss_0_port_1_pin_0_HSIOM HSIOM_SEL_GPIO
+    #define ioss_0_port_1_pin_0_HSIOM HSIOM_SEL_GPIO
 #endif
 #define CYBSP_SWDCK_HSIOM ioss_0_port_1_pin_0_HSIOM
 #define CYBSP_SWDCK_IRQ ioss_interrupts_gpio_1_IRQn
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK_HAL_PORT_PIN P1_0
+    #define CYBSP_SWDCK_HAL_PORT_PIN P1_0
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK P1_0
+    #define CYBSP_SWDCK P1_0
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK_HAL_IRQ CYHAL_GPIO_IRQ_NONE
+    #define CYBSP_SWDCK_HAL_IRQ CYHAL_GPIO_IRQ_NONE
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK_HAL_DIR CYHAL_GPIO_DIR_BIDIRECTIONAL 
+    #define CYBSP_SWDCK_HAL_DIR CYHAL_GPIO_DIR_BIDIRECTIONAL 
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDCK_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_STRONG
+    #define CYBSP_SWDCK_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_STRONG
 #endif //defined (CY_USING_HAL)
 #define CYBSP_SWDIO_ENABLED 1U
 #define CYBSP_SWDIO_PORT GPIO_PRT1
@@ -75,53 +76,53 @@ extern "C" {
 #define CYBSP_SWDIO_DRIVEMODE CY_GPIO_DM_STRONG
 #define CYBSP_SWDIO_INIT_DRIVESTATE 1
 #ifndef ioss_0_port_1_pin_4_HSIOM
-	#define ioss_0_port_1_pin_4_HSIOM HSIOM_SEL_GPIO
+    #define ioss_0_port_1_pin_4_HSIOM HSIOM_SEL_GPIO
 #endif
 #define CYBSP_SWDIO_HSIOM ioss_0_port_1_pin_4_HSIOM
 #define CYBSP_SWDIO_IRQ ioss_interrupts_gpio_1_IRQn
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO_HAL_PORT_PIN P1_4
+    #define CYBSP_SWDIO_HAL_PORT_PIN P1_4
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO P1_4
+    #define CYBSP_SWDIO P1_4
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO_HAL_IRQ CYHAL_GPIO_IRQ_NONE
+    #define CYBSP_SWDIO_HAL_IRQ CYHAL_GPIO_IRQ_NONE
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO_HAL_DIR CYHAL_GPIO_DIR_BIDIRECTIONAL 
+    #define CYBSP_SWDIO_HAL_DIR CYHAL_GPIO_DIR_BIDIRECTIONAL 
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_SWDIO_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_STRONG
+    #define CYBSP_SWDIO_HAL_DRIVEMODE CYHAL_GPIO_DRIVE_STRONG
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_USER_BTN (P2_0)
-	#define CYBSP_USER_BTN1 CYBSP_USER_BTN
+    #define CYBSP_USER_BTN (P2_0)
+    #define CYBSP_USER_BTN1 CYBSP_USER_BTN
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_USER_LED (P2_1)
-	#define CYBSP_USER_LED1 CYBSP_USER_LED
+    #define CYBSP_USER_LED (P2_1)
+    #define CYBSP_USER_LED1 CYBSP_USER_LED
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_DEBUG_UART_TX (P4_0)
+    #define CYBSP_DEBUG_UART_TX (P4_0)
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_DEBUG_UART_RX (P4_1)
+    #define CYBSP_DEBUG_UART_RX (P4_1)
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_I2C_SDA (P5_0)
+    #define CYBSP_I2C_SDA (P5_0)
 #endif //defined (CY_USING_HAL)
 #if defined (CY_USING_HAL)
-	#define CYBSP_I2C_SCL (P5_1)
+    #define CYBSP_I2C_SCL (P5_1)
 #endif //defined (CY_USING_HAL)
 
 extern const cy_stc_gpio_pin_config_t CYBSP_SWDCK_config;
 #if defined (CY_USING_HAL)
-	extern const cyhal_resource_inst_t CYBSP_SWDCK_obj;
+    extern const cyhal_resource_inst_t CYBSP_SWDCK_obj;
 #endif //defined (CY_USING_HAL)
 extern const cy_stc_gpio_pin_config_t CYBSP_SWDIO_config;
 #if defined (CY_USING_HAL)
-	extern const cyhal_resource_inst_t CYBSP_SWDIO_obj;
+    extern const cyhal_resource_inst_t CYBSP_SWDIO_obj;
 #endif //defined (CY_USING_HAL)
 
 void init_cycfg_pins(void);
